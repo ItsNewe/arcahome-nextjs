7730f144005eede47faaf2e2856c69e38f30f260
Newe <16577829+ItsNewe@users.noreply.github.com>
2025-05-27 13:00:04 +0200

    Nerfed ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+	rules: {
+	  "prefer-const": [
+		"warn",
+		{
+		  vars: "all",
+		  args: "after-used",
+		  ignoreRestSiblings: true,
+		},
+	  ],
+	  "@typescript-eslint/ban-ts-comment": "off",
+	  "@typescript-eslint/no-unused-vars": "warn",
+	  "@typescript-eslint/no-explicit-any": "warn"
+	},
+  })
 ];
 
 export default eslintConfig;
